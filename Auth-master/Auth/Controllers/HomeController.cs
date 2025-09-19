e61a8e003f054a9673fe3f027e093274c22e348d
Fabio <fabio.contarini@aluno.senai.br>
2025-09-19 15:47:30 -0300

    primeira passada

---
+++ Auth-master/Auth/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+using Auth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Auth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles =  "Admin, TI")]
+        public IActionResult Tecnologia()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Rh")]
+        public IActionResult Rh()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Financeiro")]
+        public IActionResult Financeiro()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Comercial")]
+        public IActionResult Comercial()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
